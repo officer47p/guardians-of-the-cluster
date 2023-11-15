c549d631911f5efbd77169a605e5b50cbbfc6ddd
Parsa Hosseini <officer47p@gmail.com>
2023-11-15 03:46:05 +0330

    Refactor unit tests, still not perfect but acceptable

--- rate_limiter_test.go
+++ rate_limiter_test.go
@@ -11,62 +11,178 @@ var (
 	resetInterval           = time.Second * 10 // TODO: Read from environment
 )
 
-func TestRateLimiterReturnErrorAfterHittingRequestNumberQuota(t *testing.T) {
-	token := "some-string-as-token"
+func TestRateLimiterRequestNumberQuota(t *testing.T) {
 
-	cache := NewInMemoryCache()
-	rateLimiter := NewRateLimiter(
-		&cache,
-		defaultUserRequestQuota,
-		defaultUserTrafficQuota,
-		resetInterval,
-	)
+	t.Run("should rate-limit", func(t *testing.T) {
+		token := "some-string-as-token"
+		cache := NewInMemoryCache()
+		rateLimiter := NewRateLimiter(
+			&cache,
+			defaultUserRequestQuota,
+			defaultUserTrafficQuota,
+			resetInterval,
+		)
 
-	for i := 0; i < int(defaultUserRequestQuota); i++ {
-		ok, err := rateLimiter.CanMakeRequest(token, 1)
+		for i := 0; i < int(defaultUserRequestQuota); i++ {
+			ok, err := rateLimiter.CanMakeRequest(token, 1)
+			if err != nil {
+				t.Fatalf("hit error. Err: %s", err)
+			}
+			if !ok {
+				t.Fatal("rate limited")
+			}
+		}
+
+		ok, err := rateLimiter.CanMakeRequest(token, 10)
 		if err != nil {
-			t.Fatalf("hit error. Err: %s", err)
+			t.Fatalf("hit error")
+		}
+		if ok {
+			t.Fatalf("was not rate limited")
+		}
+	})
+
+	t.Run("should recover from rate-limit after cycle reset", func(t *testing.T) {
+		token := "some-string-as-token"
+		cache := NewInMemoryCache()
+		rateLimiter := NewRateLimiter(
+			&cache,
+			defaultUserRequestQuota,
+			defaultUserTrafficQuota,
+			resetInterval,
+		)
+
+		for i := 0; i < int(defaultUserRequestQuota*2); i++ {
+			_, err := rateLimiter.CanMakeRequest(token, 0)
+			if err != nil {
+				t.Fatalf("hit error. Err: %s", err)
+			}
+		}
+
+		ok, err := rateLimiter.CanMakeRequest(token, 0)
+		if err != nil {
+			t.Fatalf("hit error")
+		}
+		if ok {
+			t.Fatalf("was not rate limited")
+		}
+
+		err = rateLimiter.ResetCycle()
+		if err != nil {
+			t.Fatalf("fail to reset cycle")
+		}
+
+		ok, err = rateLimiter.CanMakeRequest(token, 1)
+		if err != nil {
+			t.Fatalf("hit error")
 		}
 		if !ok {
-			t.Fatal("rate limited")
+			t.Fatalf("was rate limited")
 		}
-	}
-	ok, err := rateLimiter.CanMakeRequest(token, 10)
-	if err != nil {
-		t.Fatalf("hit error")
-	}
-	if ok {
-		t.Fatalf("was not rate limited")
-	}
+	})
 
 }
 
-func TestRateLimiterReturnErrorAfterHittingTrafficQuota(t *testing.T) {
-	token := "some-string-as-token"
+func TestRateLimiterTrafficQuota(t *testing.T) {
+	t.Run("should-rate-limit", func(t *testing.T) {
+		token := "some-string-as-token"
 
-	cache := NewInMemoryCache()
-	rateLimiter := NewRateLimiter(
-		&cache,
-		defaultUserRequestQuota,
-		defaultUserTrafficQuota,
-		resetInterval,
-	)
+		cache := NewInMemoryCache()
+		rateLimiter := NewRateLimiter(
+			&cache,
+			defaultUserRequestQuota,
+			defaultUserTrafficQuota,
+			resetInterval,
+		)
 
-	for i := 0; i < 2; i++ {
+		for i := 0; i < 2; i++ {
+			ok, err := rateLimiter.CanMakeRequest(token, 4)
+			if err != nil {
+				t.Fatalf("hit error. Err: %s", err)
+			}
+			if !ok {
+				t.Fatal("rate limited")
+			}
+		}
 		ok, err := rateLimiter.CanMakeRequest(token, 4)
 		if err != nil {
-			t.Fatalf("hit error. Err: %s", err)
+			t.Fatalf("hit error")
+		}
+		if ok {
+			t.Fatalf("was not rate limited")
+		}
+	})
+
+	t.Run("should recover from rate-limit after cycle reset", func(t *testing.T) {
+		token := "some-string-as-token"
+
+		cache := NewInMemoryCache()
+		rateLimiter := NewRateLimiter(
+			&cache,
+			defaultUserRequestQuota,
+			defaultUserTrafficQuota,
+			resetInterval,
+		)
+
+		for i := 0; i < 2; i++ {
+			ok, err := rateLimiter.CanMakeRequest(token, defaultUserTrafficQuota/2)
+			if err != nil {
+				t.Fatalf("hit error. Err: %s", err)
+			}
+			if !ok {
+				t.Fatal("rate limited")
+			}
+		}
+		ok, err := rateLimiter.CanMakeRequest(token, defaultUserTrafficQuota/2)
+		if err != nil {
+			t.Fatalf("hit error")
+		}
+		if ok {
+			t.Fatalf("was not rate limited")
+		}
+
+		err = rateLimiter.ResetCycle()
+		if err != nil {
+			t.Fatalf("fail to reset cycle")
+		}
+
+		ok, err = rateLimiter.CanMakeRequest(token, 1)
+		if err != nil {
+			t.Fatalf("hit error")
 		}
 		if !ok {
-			t.Fatal("rate limited")
+			t.Fatalf("was rate limited")
 		}
-	}
-	ok, err := rateLimiter.CanMakeRequest(token, 4)
-	if err != nil {
-		t.Fatalf("hit error")
-	}
-	if ok {
-		t.Fatalf("was not rate limited")
-	}
+	})
 
 }
+
+// func TestRateLimiterReturnErrorAfterHittingTrafficQuota(t *testing.T) {
+// 	token := "some-string-as-token"
+
+// 	cache := NewInMemoryCache()
+// 	rateLimiter := NewRateLimiter(
+// 		&cache,
+// 		defaultUserRequestQuota,
+// 		defaultUserTrafficQuota,
+// 		resetInterval,
+// 	)
+
+// 	for i := 0; i < 2; i++ {
+// 		ok, err := rateLimiter.CanMakeRequest(token, 4)
+// 		if err != nil {
+// 			t.Fatalf("hit error. Err: %s", err)
+// 		}
+// 		if !ok {
+// 			t.Fatal("rate limited")
+// 		}
+// 	}
+// 	ok, err := rateLimiter.CanMakeRequest(token, 4)
+// 	if err != nil {
+// 		t.Fatalf("hit error")
+// 	}
+// 	if ok {
+// 		t.Fatalf("was not rate limited")
+// 	}
+
+// }
